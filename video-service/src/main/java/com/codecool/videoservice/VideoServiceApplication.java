99e4cf095b9aafde82780e2a56d74bed06919281
BoziDaniel <daniel.bozi88@gmail.com>
2020-06-14 21:34:09 +0200

    implemented api gateway

--- video-service/src/main/java/com/codecool/videoservice/VideoServiceApplication.java
+++ video-service/src/main/java/com/codecool/videoservice/VideoServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
